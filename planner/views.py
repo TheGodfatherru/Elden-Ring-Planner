0b786d7d2fe48b1742137e1ec304ca425b52e7f0
maksim <maks12ate@gmail.com>
2022-11-28 23:03:11 +0300

    add five db table

--- planner/views.py
+++ planner/views.py
@@ -1,3 +1,11 @@
+from django.http import HttpResponseNotFound
 from django.shortcuts import render
+from .models import *
 
-# Create your views here.
+
+def index(request):
+    return render(request, 'planner/index.html')
+
+
+def pageNotFound(request, exception):
+    return HttpResponseNotFound('<h1>Страница не найдена</h1>')
